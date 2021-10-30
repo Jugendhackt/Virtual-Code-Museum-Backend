e9f67c13151ff4f150f354216b4378ef204a208d
TestTest784 <56189374+TestTest784@users.noreply.github.com>
2021-10-30 12:52:30 +0200

    okay

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/room/<int:number>")
+def loading_room(number):
+    #raum laden code hier
+        return "<p>raum " + str(number) + "</p>"
+@app.route('/submit_code', methods=['POST'])
+def submit_code():
+    f = request.files['submit_code']
+    f.save('uploaded_file.txt')
+  #code soll danach noch gesaved werden
