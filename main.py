066996b5c4aa13452676ab1f120844dfcdab8be5
TestTest784 <56189374+TestTest784@users.noreply.github.com>
2021-10-30 19:34:56 +0200

    okay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+import uuid
 
 app = Flask(__name__)
 
@@ -12,8 +13,24 @@ def hello_world():
 def loading_room(number):
     #raum laden code hier
         return "<p>raum " + str(number) + "</p>"
+
+
+
 @app.route('/submit_code', methods=['POST'])
 def submit_code():
+    dateiname = str(uuid.uuid4())
+    fileExtension = ".txt"
+    if request.json["language" ] == "python":
+        fileExtension = ".py"
+    elif request.json["language"] == "javascript":
+        fileExtension = ".js"
     f = request.files['submit_code']
-    f.save('uploaded_file.txt')
+    f.save(dateiname + fileExtension)
+    return "ok"
   #code soll danach noch gesaved werden
+
+
+@app.route('/getimage/')
+def semmel():
+    open ('semmel')
+
